d00f277a7f6a44cc081755d90c657a39549e2ef5
aopontann <aopontan0416@gmail.com>
2022-03-30 14:54:51 +0000

    feat: json書き出し機能の実装

--- seed.go
+++ seed.go
@@ -20,7 +20,7 @@ type jsonList struct {
 
 func Seed(w http.ResponseWriter, _ *http.Request) {
 	var vtuberList jsonList
-	raw, err := ioutil.ReadFile("./sample_VtuberList.json")
+	raw, err := ioutil.ReadFile("./vtuberList.json")
     if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
@@ -37,4 +37,30 @@ func Seed(w http.ResponseWriter, _ *http.Request) {
 			log.Fatal(err)
 		}
 	}
+}
+
+func SeedOut(w http.ResponseWriter, _ *http.Request) {
+	var (
+		id string
+		name string
+	)
+	var vtuberList jsonList
+	rows, err := DB.Query("select id, name from vtubers")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		vtuberList.List = append(vtuberList.List, vtuberInfo{Id: id, Name: name})
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, _ := json.MarshalIndent(vtuberList, "", " ")
+	_ = ioutil.WriteFile("vtuberList.json", file, 0644)
 }
