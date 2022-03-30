92b2b95ab0c4df88bb9ae525bbea4880455dafab
aopontann <aopontan0416@gmail.com>
2022-03-30 14:56:50 +0000

    refactor: 余分なエンドポイントを削除

--- main.go
+++ main.go
@@ -22,14 +22,11 @@ func main() {
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello from a HandleFunc #1!!!\n")
 	}
-	h2 := func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello from a HandleFunc #2!\n")
-	}
 
 	http.HandleFunc("/", h1)
-	http.HandleFunc("/endpoint", h2)
 	http.HandleFunc("/youtube", YoutubeHandler)
-	http.HandleFunc("/seed", Seed) // Seed
+	// http.HandleFunc("/seed", Seed) // Seed
+	// http.HandleFunc("/seedOut", SeedOut) // Seed
 
 	port := os.Getenv("PORT")
 	if port == "" {
