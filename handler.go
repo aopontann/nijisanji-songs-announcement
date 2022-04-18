dfdb2825712b63fc7301e3576a1f3bee6f63d6d0
aopontann <aopontan0416@gmail.com>
2022-04-18 10:52:48 +0000

    refactor: ファイル分割した

---
+++ handler.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func YoutubeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+        w.WriteHeader(http.StatusMethodNotAllowed) // 405
+        w.Write([]byte("POSTだけだよ"))
+        return
+    }
+
+	ctx := context.Background()
+	videoId, err := YoutubeSearchList(ctx)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	fmt.Printf("videoId=%s\n", videoId)
+
+	err = YoutubeVideoList(ctx, videoId)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte("Youtube OK"))
+}
+
+const endpoint = "https://api.twitter.com/2/tweets"
+
+func TwitterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        w.Write([]byte("POSTだけだよ"))
+        return
+    }
+	dtAfter := time.Now().Format("2006-01-02 15:04:00")
+	dtBefore := time.Now().Add(5 * time.Minute).Format("2006-01-02 15:04:00")
+
+	videoList, err := GetVideos(dtAfter, dtBefore)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	for _, video := range videoList {
+		err := PostTweet(video.Id, video.Title)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+	}
+	w.Write([]byte("Twitter OK"))
+}
