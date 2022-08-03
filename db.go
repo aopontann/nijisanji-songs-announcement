5b6f9b5566ecc5ea6fe447c42fcb0ef175e43825
aopontann <aopontan0416@gmail.com>
2022-08-03 15:32:46 +0000

    firestoreにツイートIDと動画ID、公開時間を保存する機能を実装

--- db.go
+++ db.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/rs/zerolog/log"
 )
+
+type FireStore struct{}
 
 var DB *sql.DB
 
@@ -21,4 +24,19 @@ func DBInit() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("db ping failed")
 	}
+}
+
+func (fs *FireStore) Save(ytcr []YouTubeCheckResponse) error {
+	ctx := context.Background()
+	for _, v := range ytcr {
+		_, _, err := FireStoreService.Collection("demo").Add(ctx, map[string]interface{}{
+			"twitter_id":  v.TwitterID,
+			"schedule": v.Schedule,
+			"youtube_id":   v.ID,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
