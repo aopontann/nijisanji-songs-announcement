d5e647c44cec10fe05f95a85b61d8c0cd931a2b4
aopontann <aopontan0416@gmail.com>
2023-04-03 14:21:17 +0000

    動画検索範囲の時間帯を廃止し、過去のアクティビティイベント5回分取得するように変更 ライバーがとある時間にイベントを起こしても、その時間帯の情報を取得しようとしても取得できない時があった(APIの仕様上?)ため、 あえて時間設定せず、リクエストを送るように変更した。 #51

--- youtube.go
+++ youtube.go
@@ -259,9 +259,6 @@ func (yt *Youtube) CheckVideo(vid string) (bool, error) {
 }
 
 func Activities() (VideoIDList, error) {
-	// 動画検索範囲
-	dtAfter := time.Now().UTC().Add(-120 * time.Minute).Format("2006-01-02T15:04:00Z")
-	dtBefore := time.Now().UTC().Format("2006-01-02T15:04:00Z")
 	// 動画IDを格納する文字列型配列を宣言
 	vid := make([]string, 0, 600)
 
@@ -271,36 +268,31 @@ func Activities() (VideoIDList, error) {
 		return nil, err
 	}
 
-	pt := ""
 	for _, cid := range channelIdList {
-		for {
-			call := YouTubeService.Activities.List([]string{"snippet", "contentDetails"}).ChannelId(cid).MaxResults(50).PublishedAfter(dtAfter).PublishedBefore(dtBefore).PageToken(pt)
-			res, err := call.Do()
-			if err != nil {
-				log.Error().Str("severity", "ERROR").Err(err).Msg("activities-list call error")
-				return []string{}, err
-			}
-
-			for _, item := range res.Items {
-				if item.Snippet.Type != "upload" {
-					continue
-				}
-				vid = append(vid, item.ContentDetails.Upload.VideoId)
-			}
-
-			log.Info().
-				Str("severity", "INFO").
-				Str("service", "youtube-activities-list").
-				Str("published", fmt.Sprintf("%s ~ %s", dtAfter, dtBefore)).
-				Str("ChannelId", cid).
-				Str("pageInfo", fmt.Sprintf("perPage=%d total=%d nextPage=%s\n", res.PageInfo.ResultsPerPage, res.PageInfo.TotalResults, res.NextPageToken)).
-				Send()
-
-			if res.NextPageToken == "" {
-				break
-			}
-			pt = res.NextPageToken
+		// 取得した動画IDをログに出力するための変数
+		var rid []string
+		call := YouTubeService.Activities.List([]string{"snippet", "contentDetails"}).ChannelId(cid).MaxResults(5)
+		res, err := call.Do()
+		if err != nil {
+			log.Error().Str("severity", "ERROR").Err(err).Msg("activities-list call error")
+			return []string{}, err
 		}
+
+		for _, item := range res.Items {
+			if item.Snippet.Type != "upload" {
+				continue
+			}
+			rid = append(rid, item.ContentDetails.Upload.VideoId)
+			vid = append(vid, item.ContentDetails.Upload.VideoId)
+		}
+
+		log.Info().
+			Str("severity", "INFO").
+			Str("service", "youtube-activities-list").
+			Str("ChannelId", cid).
+			Strs("rid", rid).
+			Str("pageInfo", fmt.Sprintf("perPage=%d total=%d nextPage=%s\n", res.PageInfo.ResultsPerPage, res.PageInfo.TotalResults, res.NextPageToken)).
+			Send()
 	}
 	return vid, nil
 }
