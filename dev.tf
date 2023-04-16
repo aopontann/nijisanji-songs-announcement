780b4cfbd74b8aabf4b4d89d4ded9213c0d553b2
aopontann <aopontan0416@gmail.com>
2023-04-16 15:02:43 +0000

    nsa_bot_dev_check_new_video 作成 #59 歌ってみた動画が新しくアップロードされているかチェックするschedulerの設定を追加

--- dev.tf
+++ dev.tf
@@ -4,6 +4,7 @@ resource "google_cloud_scheduler_job" "nsa_bot_dev_tweet" {
   schedule         = "*/5 * * * *"
   time_zone        = "Asia/Tokyo"
   attempt_deadline = "300s"
+  paused = true
 
   retry_config {
     retry_count = 1
@@ -12,5 +13,22 @@ resource "google_cloud_scheduler_job" "nsa_bot_dev_tweet" {
   http_target {
     http_method = "POST"
     uri         = "${var.cloud_run_service_url_dev}/twitter"
+  }
+}
+
+resource "google_cloud_scheduler_job" "nsa_bot_dev_check_new_video" {
+  name             = "nsa-bot-dev-check-new-video"
+  description      = "歌ってみた動画が新しくアップロードされているかチェックする"
+  schedule         = "*/5 * * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "300s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "${var.cloud_run_service_url_dev}/youtube/checkNewVideo"
   }
 }
