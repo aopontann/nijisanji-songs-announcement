9098e0f799e5570bbee4b703e1d85e7f00d0ddad
aopontann <aopontan0416@gmail.com>
2023-04-18 04:35:29 +0000

    プレイリストIDを登録する関数の作成

--- db.go
+++ db.go
@@ -22,3 +22,38 @@ func DBInit() {
 		log.Fatal().Err(err).Msg("db ping failed")
 	}
 }
+
+func PlaylistsSeed() error {
+	cList, err := Channels()
+	if err != nil {
+		return err
+	}
+
+	tx, err := DB.Begin()
+	if err != nil {
+		log.Error().Str("severity", "ERROR").Err(err).Msg("DB.Begin error")
+		return err
+	}
+
+	stmt, err := tx.Prepare("UPDATE vtubers SET playlist_id = ? WHERE id = ?")
+	if err != nil {
+		log.Error().Str("severity", "ERROR").Err(err).Msg("DB.Prepare error")
+		return err
+	}
+
+	for _, c := range cList {
+		_, err = stmt.Exec(c.PlaylistID, c.ID)
+		if err != nil {
+			log.Error().Str("severity", "ERROR").Err(err).Msg("DB.Exec error")
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Error().Str("severity", "ERROR").Err(err).Msg("tx.commit error")
+		return err
+	}
+
+	return nil
+}
