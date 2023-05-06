d31414768c430e4020fa405bea051e1a90f6425f
aopontann <aopontan0416@gmail.com>
2023-05-06 09:32:31 +0000

    ポートリッスン部分やハンドラ関数の登録処理部分を削除&Taskの関数を実行するように変更

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"os"
-
-	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	log.Debug().Str("severity", "DEBUG").Str("PORT", port).Send()
-	if port == "" {
-		port = "8080"
-	}
-
 	// YouTube Data API 初期化
 	YTNew()
 
@@ -22,36 +12,14 @@ func main() {
 	DBInit()
 	defer DB.Close()
 
-	h1 := func(w http.ResponseWriter, _ *http.Request) {
-		log.Info().Str("severity", "INFO").Msg("pong!!!")
-		io.WriteString(w, "pong\n")
+	taskNum := os.Getenv("CLOUD_RUN_TASK_INDEX")
+	if taskNum == "0" {
+		CheckNewVideoTask()
 	}
-
-	send := func(w http.ResponseWriter, _ *http.Request) {
-		SendMail("test-subject", "test2-message")
-		io.WriteString(w, "send-demo\n")
-	}
-
-	tweet := func(w http.ResponseWriter, _ *http.Request) {
-		video := GetVideoInfo{ID: "test", Title: "test"}
-		err := video.Tweets()
-		if err != nil {
-			io.WriteString(w, "failed-tweet\n")
-			return
-		}
-		io.WriteString(w, "success-tweet\n")
-	}
-
-	http.HandleFunc("/tweet", TweetHandler)
-	http.HandleFunc("/item-count", UpdateItemCountHandler) // PUT
-	http.HandleFunc("/check-new-video", CheckNewVideoHAndler) // POST
-
-	// 検証用
-	http.HandleFunc("/ping", h1)
-	http.HandleFunc("/mail", send)
-	http.HandleFunc("/test/tweet", tweet)
-
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal().Err(err).Msg("start http server failed")
+	if taskNum == "1" {
+		TweetTask()
+	} else {
+		CheckNewVideoTask()
+		TweetTask()
 	}
 }
