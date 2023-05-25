423e3a05af7e9abf0cf745a092761d21ed9052fd
aopontann <aopontan0416@gmail.com>
2023-05-25 12:01:25 +0000

    refactor: twitterとmailパッケージを使うように変更 #69

--- task.go
+++ task.go
@@ -8,7 +8,9 @@ import (
 	"time"
 
 	"github.com/aopontann/nijisanji-songs-announcement/cmd/selection"
+	"github.com/aopontann/nijisanji-songs-announcement/cmd/twitter"
 	"github.com/aopontann/nijisanji-songs-announcement/cmd/youtube"
+	"github.com/aopontann/nijisanji-songs-announcement/cmd/mail"
 	ndb "github.com/aopontann/nijisanji-songs-announcement/db"
 	"github.com/rs/zerolog/log"
 )
@@ -27,7 +29,7 @@ func UpdateItemCountTask(db *sql.DB) error {
 	ctx := context.Background()
 	queries := ndb.New(db)
 
-	tx, err := DB.Begin()
+	tx, err := db.Begin()
 	if err != nil {
 		return fmt.Errorf("DB.Begin failed")
 	}
@@ -36,8 +38,8 @@ func UpdateItemCountTask(db *sql.DB) error {
 	// 動画が削除されて動画数が減っていても、上書きする
 	for pid, count := range itemCountList {
 		err = qtx.UpdatePlaylistItemCount(ctx, ndb.UpdatePlaylistItemCountParams{
-			ItemCount: int32(count),
-			ID: pid,
+			ItemCount:   int32(count),
+			ID:          pid,
 			ItemCount_2: int32(count),
 		})
 		if err != nil {
@@ -92,9 +94,9 @@ func CheckNewVideoTask(db *sql.DB) error {
 	for _, v := range vlist {
 		var err error
 		if os.Getenv("ENV") == "dev" {
-			err = SendMail("【開発用】新しい動画がアップロードされました", fmt.Sprintf("https://www.youtube.com/watch?v=%s", v.Id))
+			err = mail.Send("【開発用】新しい動画がアップロードされました", fmt.Sprintf("https://www.youtube.com/watch?v=%s", v.Id))
 		} else {
-			err = SendMail("新しい動画がアップロードされました", fmt.Sprintf("https://www.youtube.com/watch?v=%s", v.Id))
+			err = mail.Send("新しい動画がアップロードされました", fmt.Sprintf("https://www.youtube.com/watch?v=%s", v.Id))
 		}
 		if err != nil {
 			return err
@@ -104,7 +106,7 @@ func CheckNewVideoTask(db *sql.DB) error {
 	ctx := context.Background()
 	queries := ndb.New(db)
 
-	tx, err := DB.Begin()
+	tx, err := db.Begin()
 	if err != nil {
 		return fmt.Errorf("DB.Begin failed")
 	}
@@ -130,8 +132,8 @@ func CheckNewVideoTask(db *sql.DB) error {
 	// 動画が削除されて動画数が減っていても、上書きする
 	for pid, count := range itemCountList {
 		err = qtx.UpdatePlaylistItemCount(ctx, ndb.UpdatePlaylistItemCountParams{
-			ItemCount: int32(count),
-			ID: pid,
+			ItemCount:   int32(count),
+			ID:          pid,
 			ItemCount_2: int32(count),
 		})
 		if err != nil {
@@ -150,27 +152,32 @@ func CheckNewVideoTask(db *sql.DB) error {
 	return nil
 }
 
-func TweetTask() error {
-	dtAfter := time.Now().UTC().Add(1 * time.Second).Format("2006-01-02 15:04:05")
-	dtBefore := time.Now().UTC().Add(5 * time.Minute).Format("2006-01-02 15:04:00")
+func TweetTask(db *sql.DB) error {
+	queries := ndb.New(db)
+	tAfter, _ := time.Parse(time.RFC3339, time.Now().UTC().Format("2006-01-02T15:04:00Z"))
+	tBefore := tAfter.Add(5 * time.Minute)
 
-	log.Info().Str("severity", "INFO").Str("service", "tweet").Str("datetime", fmt.Sprintf("%s ~ %s\n", dtAfter, dtBefore)).Send()
+	log.Info().Str("severity", "INFO").Str("service", "tweet").Str("datetime", fmt.Sprintf("%v ~ %v\n", tAfter, tBefore)).Send()
 
-	videoList, err := GetVideos(dtAfter, dtBefore)
+	ctx := context.Background()
+	vList, err := queries.ListVideoIdTitle(ctx, ndb.ListVideoIdTitleParams{
+		ScheduledStartTime: tAfter,
+		ScheduledStartTime_2: tBefore,
+	})
 	if err != nil {
 		return err
 	}
 
-	for _, video := range videoList {
+	tw := twitter.New()
+
+	for _, video := range vList {
 		// changed, err := yt.CheckVideo(video.Id)
 		log.Info().Str("severity", "INFO").Str("service", "tweet").Str("id", video.ID).Str("title", video.Title).Send()
 		if err != nil {
 			return err
 		}
-		// if changed {
-		// 	continue
-		// }
-		err = video.Tweets()
+
+		err = tw.Id(video.ID).Title(video.Title).Tweet()
 		if err != nil {
 			return err
 		}
