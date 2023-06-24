3f4b2130df6be83dc07a6a5c093f24b97786aae2
aopontann <aopontan0416@gmail.com>
2023-06-24 08:56:51 +0000

    ✨ misskeyで告知するように実装

---
+++ cmd/misskey/main.go
@@ -0,0 +1,45 @@
+package misskey
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const url = "https://@aopontan@misskey.io/api/notes/create"
+
+type Misskey struct {
+	token string
+}
+
+type ReqBody struct {
+	I      string `json:"i"`
+	Text   string `json:"text"`
+	Detail bool   `json:"detail"`
+}
+
+func New(token string) *Misskey {
+	return &Misskey{token: token}
+}
+
+func (m *Misskey) Post(id string, title string) error {
+	content := fmt.Sprintf(`{"text": "【5分後に公開】\n\n%s\n\nhttps://www.youtube.com/watch?v=%s"}`, title, id)
+	resb := ReqBody{
+		I:      m.token,
+		Text:   content,
+		Detail: false,
+	}
+
+	payload, err := json.Marshal(resb)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
