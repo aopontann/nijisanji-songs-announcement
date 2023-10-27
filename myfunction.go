c58413a87a996a591005882d89220e14e8631fb8
aopontann <aopontan0416@gmail.com>
2023-10-27 12:50:25 +0000

    cloud functions で使われるエントリーポイントを作成 #74

---
+++ myfunction.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"database/sql"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/zerolog/log"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	// Register an HTTP function with the Functions Framework
+	functions.HTTP("MyHTTPFunction", myHTTPFunction)
+}
+
+// Function myHTTPFunction is an HTTP handler
+func myHTTPFunction(w http.ResponseWriter, r *http.Request) {
+	// Your code here
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatal().Str("severity", "ERROR").Str("service", "sql.Open").Msg(err.Error())
+	}
+
+	err = CheckNewVideoTask(db)
+	if err != nil {
+		log.Fatal().Str("severity", "ERROR").Msg(err.Error())
+	}
+	err = MisskeyPostTask(db)
+	if err != nil {
+		log.Fatal().Str("severity", "ERROR").Msg(err.Error())
+	}
+	err = TweetTask(db)
+	if err != nil {
+		log.Fatal().Str("severity", "ERROR").Msg(err.Error())
+	}
+
+	// Send an HTTP response
+	fmt.Fprintln(w, "OK")
+}
