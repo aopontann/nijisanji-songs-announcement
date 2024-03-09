799f083d03519ae353fdd0d3f57715c4513709a6
aopontann <aopontan0416@gmail.com>
2024-03-09 08:56:44 +0000

    全処理を一新したものを使うように修正

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,11 +12,59 @@ import (
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger) // 以降、JSON形式で出力される。
-	http.HandleFunc("/hello", nsa.Hello)
-	http.HandleFunc("/check", nsa.CheckNewVideo)
-	http.HandleFunc("/video", nsa.SaveVideos)
-	http.HandleFunc("/song", nsa.SongVideoAnnounce)
-	http.HandleFunc("/keyword", nsa.KeywordAnnounce)
+
+	http.HandleFunc("/v2/check", func(w http.ResponseWriter, r *http.Request) {
+		err := nsa.CheckNewVideoJob()
+		if err != nil {
+			slog.Error("CheckNewVideoJob",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/v2/keyword", func(w http.ResponseWriter, r *http.Request) {
+		err := nsa.KeywordAnnounceJob()
+		if err != nil {
+			slog.Error("KeywordAnnounceJob",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/v2/song", func(w http.ResponseWriter, r *http.Request) {
+		err := nsa.SongVideoAnnounceJob()
+		if err != nil {
+			slog.Error("SongVideoAnnounceJob",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/v2/delete", func(w http.ResponseWriter, r *http.Request) {
+		err := nsa.DeleteVideoJob()
+		if err != nil {
+			slog.Error("DeleteVideoJob",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/v2/playlistItems/update", func(w http.ResponseWriter, r *http.Request) {
+		err := nsa.UpdatePlaylistItemJob()
+		if err != nil {
+			slog.Error("UpdatePlaylistItemJob",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
 	if port == "" {
