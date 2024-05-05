e710b56790e1c623b2d37b001b95506fd30fe9a2
aopontann <aopontan0416@gmail.com>
2024-05-05 06:33:28 +0000

    キーワード登録実装、その他修正 #91

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,4 +1,9 @@
-const checkbox = document.getElementById('subscribe')
+const msgEle = document.getElementById('msg')
+const songEle = document.getElementById('song')
+const keywordEle = document.getElementById('keyword')
+const keywordTextEle = document.getElementById('keyword-text')
+const submitEle = document.getElementById('submit')
+const deleteEle = document.getElementById('delete')
 const confEle = document.getElementById('subscription-conf')
 const loadingEle = document.getElementById('loading')
 
@@ -6,45 +11,103 @@ if (window.navigator.serviceWorker !== undefined) {
   window.navigator.serviceWorker.register('/serviceworker.js');
 }
 
-window.onload = async() => {
+window.onload = async () => {
   console.log("page is fully loaded");
 
-  checkbox.addEventListener('change', async(event) => {
-    console.log("AAA")
-    checkbox.disabled = true
-    if (event?.currentTarget?.checked) {
-      await subscribe()
-    } else {
-      await unSubscribe()
-    }
-    checkbox.disabled = false
-  })
+  // iPhone, iPad の場合
+  if (isIos() && !isInStandaloneMode()) {
+    window.alert("iPhone, iPad, iPodをお使いの場合、「共有」→「ホーム画面に追加」して、追加されたアイコンから起動してください")
+    msgEle.innerText = "iPhone, iPad, iPodをお使いの場合、「共有」→「ホーム画面に追加」して、追加されたアイコンから起動してください"
+    return
+  } 
 
-  if(!await checkIsWebPushSupported()) {
-    window.alert('「共有」→「ホーム画面に追加」して、追加されたアイコンから起動してください')
+  // サービスワーカー or WebPush に対応していない場合
+  if (!await checkIsWebPushSupported()) {
+    window.alert("お使いのブラウザでは、サービスワーカー or WebPush に対応していません")
+    msgEle.innerText = "お使いのブラウザでは、サービスワーカー or WebPush に対応していません"
+  return
+  }
+
+  // 通知許可を求める
+  if (!await IsNotificationAllowed()) {
     return
   }
-  try {
-    const registration = await navigator.serviceWorker.ready
-    const subscription = await registration.pushManager.getSubscription()
-    if (!subscription) {
-      confEle.style.display = 'block'
-      loadingEle.style.display = 'none'
+
+  const registration = await navigator.serviceWorker.ready
+
+  // Pushマネージャーから登録済み購買情報を取得
+  // 取得できない場合、Pushサーバーに購買情報を登録
+  const subscription = await registration.pushManager.getSubscription() || await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: "BCSvj0H4g72CXuyK_CUy2oygQyRXDyX_BaR2ACtfmEYm2jLj-qCymSnDhfp7acuBISkKxj_UC1TKd6eOPcfr27w",
+  })
+
+  submitEle.addEventListener('click', async () => {
+    console.log("POST")
+    loadingEle.style.display = 'block'
+    const isOK = await fetchData("POST", subscription.toJSON(), { song: songEle.checked ? 1 : 0, word: keywordEle.checked && keywordTextEle.value || "" })
+    if (!isOK) {
+      window.alert('トークンの登録に失敗しました')
+      console.log("トークンの登録に失敗しました")
+    }
+    loadingEle.style.display = 'none'
+  })
+
+  deleteEle.addEventListener("click", async () => {
+    console.log("DELETE")
+    loadingEle.style.display = 'block'
+    const isOK = await fetchData("DELETE", subscription.toJSON())
+    if (!isOK) {
+      window.alert('トークンの削除に失敗しました')
+      console.log("トークンの削除に失敗しました")
       return
     }
-    const isOK = await postData('/api/subscription/check', subscription.toJSON())
-    console.log('isOK:', isOK)
-    if (isOK) {
-      checkbox.checked = true
-    } else {
-      checkbox.checked = false
-    }
-  } catch (error) {
-      window.alert('予期せぬエラーが発生しました')
-      checkbox.checked = false
+    loadingEle.style.display = 'none'
+    songEle.checked = false
+    keywordEle.checked = false
+  })
+
+  keywordTextEle.addEventListener("change", (event) => {
+    window.localStorage.setItem("word", event.target.value)
+  })
+
+  // APIサーバーから購買情報を取得　歌ってみた動画を通知する許可をしているか...
+  const res = await isSubscription(subscription.toJSON())
+  if (res.status == 404) {
+    confEle.style.display = 'block'
+    loadingEle.style.display = 'none'
+    keywordTextEle.value = window.localStorage.getItem("word")
+    return
+  } else if (!res.ok) {
+    window.alert("購買情報の取得に失敗しました。")
+    return
   }
+
+  // 最新の購買情報に応じて要素を変更
+  const data = await res.json()
+  if (data.song == 1) {
+    songEle.checked = true
+  }
+  if (data.word != '') {
+    keywordTextEle.value = data.word
+    keywordEle.checked = true
+  }
+  if (data.word == '') {
+    keywordTextEle.value = window.localStorage.getItem("word")
+  }
+
   confEle.style.display = 'block'
   loadingEle.style.display = 'none'
+};
+
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
+};
+
+// check if the device is in standalone mode
+const isInStandaloneMode = () => {
+  return ("standalone" in window.navigator && window.navigator.standalone);
 };
 
 const checkIsWebPushSupported = async () => {
@@ -72,6 +135,7 @@ const IsNotificationAllowed = async () => {
   const permission = await window.Notification.requestPermission()
   if (permission === 'denied') {
     window.alert('通知がブロックされています')
+    msgEle.innerText = "通知がブロックされています"
     return false
   }
   else if (permission === 'granted') {
@@ -81,98 +145,42 @@ const IsNotificationAllowed = async () => {
   }
   else if (permission === 'default') {
     window.alert('通知を許可してください')
+    msgEle.innerText = "通知を許可してください"
     return false
   }
   else {
     console.log('Unable to get permission to notify.');
+    msgEle.innerText = "Unable to get permission to notify."
     return false
   }
 }
 
-async function subscribe() {
-  if (!(await IsNotificationAllowed())) {
-    checkbox.checked = false
-    return
-  }
-  console.log("subscribe")
-  const registration = await navigator.serviceWorker.ready
-  try {
-    const currentLocalSubscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: "BCSvj0H4g72CXuyK_CUy2oygQyRXDyX_BaR2ACtfmEYm2jLj-qCymSnDhfp7acuBISkKxj_UC1TKd6eOPcfr27w",
-    })
-    console.log('currentLocalSubscription:', currentLocalSubscription.toJSON())
-    const isOK = await postData('/api/subscription', currentLocalSubscription.toJSON())
-    if (isOK) {
-      checkbox.checked = true
-    }
-    else {
-      checkbox.checked = false
-      console.log("トークンの登録に失敗しました")
-    }
-  } catch (error) {
-    console.log(error)
-    checkbox.checked = false
-  }
-}
-
-async function unSubscribe() {
-  console.log("unSubscribe")
-  const registration = await navigator.serviceWorker.ready
-  const subscription = await registration.pushManager.getSubscription()
-  try {
-    if (!(await subscription?.unsubscribe())) {
-      console.log("通知の解除に失敗しました")
-      checkbox.checked = true
-      return
-    }
-    const isOK = await deleteData('/api/subscription', subscription?.toJSON())
-    if (isOK) {
-      checkbox.checked = false
-    }
-    else {
-      checkbox.checked = true
-      console.log("トークンの削除に失敗しました")
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function postData(url = "", data = {}) {
-  // 既定のオプションには * が付いています
+async function isSubscription(token) {
+  const url = "/api/subscription"
   const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    method: "GET",
+    cache: "no-cache",
+    headers: {
+      "Token": JSON.stringify(token),
+    },
+  });
+
+  return response
+}
+
+async function fetchData(method = "", token, data = {}) {
+  const url = "/api/subscription"
+  const response = await fetch(url, {
+    method,
+    cache: "no-cache",
     headers: {
       "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+      "Token": JSON.stringify(token),
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // 本体のデータ型は "Content-Type" ヘッダーと一致させる必要があります
   });
+
   console.log(response.status)
   console.log(response.ok)
-  return response.ok; // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
-}
-
-async function deleteData(url = "", data = {}) {
-  // 既定のオプションには * が付いています
-  const response = await fetch(url, {
-    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // 本体のデータ型は "Content-Type" ヘッダーと一致させる必要があります
-  });
   return response.ok; // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
 }
