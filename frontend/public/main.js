b6d9b20a335f1667ea8c2e5ab8c6a73efd94b0c1
aopontann <aopontan0416@gmail.com>
2024-05-21 13:23:08 +0000

    tailwind廃止＆リファクタ #91

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,3 +1,4 @@
+const permissionEle = document.getElementById('permission')
 const msgEle = document.getElementById('msg')
 const songEle = document.getElementById('song')
 const keywordEle = document.getElementById('keyword')
@@ -6,63 +7,102 @@ const submitEle = document.getElementById('submit')
 const deleteEle = document.getElementById('delete')
 const confEle = document.getElementById('subscription-conf')
 const loadingEle = document.getElementById('loading')
+const dialogEle = document.getElementById("favDialog");
+const okEle = document.getElementById("ok");
 
 if (window.navigator.serviceWorker !== undefined) {
   window.navigator.serviceWorker.register('/serviceworker.js');
 }
 
+// ローディング画面の実装
+
 window.onload = async () => {
   console.log("page is fully loaded");
 
+  okEle.addEventListener("click", () => {
+    window.localStorage.setItem("readed-terms", "true")
+    dialogEle.close()
+  })
+
+  if (window.localStorage.getItem("readed-terms") !== "true") {
+    dialogEle.showModal();
+  }
+
   // iPhone, iPad の場合
   if (isIos() && !isInStandaloneMode()) {
-    window.alert("iPhone, iPad, iPodをお使いの場合、「共有」→「ホーム画面に追加」して、追加されたアイコンから起動してください")
     msgEle.innerText = "iPhone, iPad, iPodをお使いの場合、「共有」→「ホーム画面に追加」して、追加されたアイコンから起動してください"
     return
-  } 
-
-  // サービスワーカー or WebPush に対応していない場合
-  if (!await checkIsWebPushSupported()) {
-    window.alert("お使いのブラウザでは、サービスワーカー or WebPush に対応していません")
-    msgEle.innerText = "お使いのブラウザでは、サービスワーカー or WebPush に対応していません"
-  return
   }
 
-  // 通知許可を求める
-  if (!await IsNotificationAllowed()) {
+  // ブラウザーが通知に対応しているか調べる
+  if (!("Notification" in window)) {
+    window.alert("このブラウザーはデスクトップ通知には対応していません。");
     return
   }
+
+  // 通知の許可が既に得られている場合
+  if (Notification.permission === "granted") {
+    // 何もしない (o.o)
+  }
+
+  // 通知権限がブロックされている場合
+  if (Notification.permission === "denied") {
+    window.alert('通知がブロックされています')
+    return
+  }
+
+  // 通知権限がブロックされていないが、ユーザーの許可を得れていない場合
+  if (Notification.permission === "default") {
+    permissionEle.style.display = "block"
+    msgEle.innerText = "↑の通知許可ボタンを押して、通知を許可してください"
+
+    permissionEle.addEventListener("click", async () => {
+      // 通知許可を求める
+      await Notification.requestPermission()
+      window.location.reload()
+    })
+    return
+  }
+
+  msgEle.style.display = "none"
+  document.getElementById("overlay").style.display = "block"
 
   const registration = await navigator.serviceWorker.ready
 
-  // Pushマネージャーから登録済み購買情報を取得
-  // 取得できない場合、Pushサーバーに購買情報を登録
-  const subscription = await registration.pushManager.getSubscription() || await registration.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: "BCSvj0H4g72CXuyK_CUy2oygQyRXDyX_BaR2ACtfmEYm2jLj-qCymSnDhfp7acuBISkKxj_UC1TKd6eOPcfr27w",
-  })
-
   submitEle.addEventListener('click', async () => {
     console.log("POST")
-    loadingEle.style.display = 'block'
+
+    document.getElementById("overlay").style.display = "block"
+
+    const subscription = await registration.pushManager.getSubscription() || await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: "BCSvj0H4g72CXuyK_CUy2oygQyRXDyX_BaR2ACtfmEYm2jLj-qCymSnDhfp7acuBISkKxj_UC1TKd6eOPcfr27w",
+    })
+
     const isOK = await fetchData("POST", subscription.toJSON(), { song: songEle.checked ? 1 : 0, word: keywordEle.checked && keywordTextEle.value || "" })
     if (!isOK) {
       window.alert('トークンの登録に失敗しました')
       console.log("トークンの登録に失敗しました")
     }
-    loadingEle.style.display = 'none'
+    document.getElementById("overlay").style.display = "none"
   })
 
   deleteEle.addEventListener("click", async () => {
     console.log("DELETE")
-    loadingEle.style.display = 'block'
+
+    const subscription = await registration.pushManager.getSubscription() || await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: "BCSvj0H4g72CXuyK_CUy2oygQyRXDyX_BaR2ACtfmEYm2jLj-qCymSnDhfp7acuBISkKxj_UC1TKd6eOPcfr27w",
+    })
+
+    document.getElementById("overlay").style.display = "block"
     const isOK = await fetchData("DELETE", subscription.toJSON())
     if (!isOK) {
       window.alert('トークンの削除に失敗しました')
       console.log("トークンの削除に失敗しました")
       return
     }
-    loadingEle.style.display = 'none'
+    document.getElementById("overlay").style.display = "none"
     songEle.checked = false
     keywordEle.checked = false
   })
@@ -71,11 +111,20 @@ window.onload = async () => {
     window.localStorage.setItem("word", event.target.value)
   })
 
+  // 既に購買済みか
+  const subscription = await registration.pushManager.getSubscription()
+  console.log("subscription:", subscription)
+  if (subscription == null) {
+    confEle.style.display = 'block'
+    document.getElementById("overlay").style.display = "none"
+    return
+  }
+
   // APIサーバーから購買情報を取得　歌ってみた動画を通知する許可をしているか...
   const res = await isSubscription(subscription.toJSON())
   if (res.status == 404) {
     confEle.style.display = 'block'
-    loadingEle.style.display = 'none'
+    document.getElementById("overlay").style.display = "none"
     keywordTextEle.value = window.localStorage.getItem("word")
     return
   } else if (!res.ok) {
@@ -97,7 +146,7 @@ window.onload = async () => {
   }
 
   confEle.style.display = 'block'
-  loadingEle.style.display = 'none'
+  document.getElementById("overlay").style.display = "none"
 };
 
 const isIos = () => {
@@ -107,53 +156,8 @@ const isIos = () => {
 
 // check if the device is in standalone mode
 const isInStandaloneMode = () => {
-  return ("standalone" in window.navigator && window.navigator.standalone);
+  return window.matchMedia('(display-mode: standalone)').matches;
 };
-
-const checkIsWebPushSupported = async () => {
-  // グローバル空間にNotificationがあればNotification APIに対応しているとみなす
-  if (!('Notification' in window)) {
-    return false;
-  }
-  // グローバル変数navigatorにserviceWorkerプロパティがあればサービスワーカーに対応しているとみなす
-  if (!('serviceWorker' in navigator)) {
-    return false;
-  }
-  try {
-    const sw = await navigator.serviceWorker.ready;
-    // 利用可能になったサービスワーカーがpushManagerプロパティがあればPush APIに対応しているとみなす
-    if (!('pushManager' in sw)) {
-      return false;
-    }
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
-
-const IsNotificationAllowed = async () => {
-  const permission = await window.Notification.requestPermission()
-  if (permission === 'denied') {
-    window.alert('通知がブロックされています')
-    msgEle.innerText = "通知がブロックされています"
-    return false
-  }
-  else if (permission === 'granted') {
-    const registration = await navigator.serviceWorker.ready
-    console.log("サービスワーカーがアクティブ:", registration.active);
-    return true
-  }
-  else if (permission === 'default') {
-    window.alert('通知を許可してください')
-    msgEle.innerText = "通知を許可してください"
-    return false
-  }
-  else {
-    console.log('Unable to get permission to notify.');
-    msgEle.innerText = "Unable to get permission to notify."
-    return false
-  }
-}
 
 async function isSubscription(token) {
   const url = "/api/subscription"
