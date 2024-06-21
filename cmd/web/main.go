26e5b07140df398b5d1760b4c4976b63b8fe9481
aopontann <aopontan0416@gmail.com>
2024-06-22 00:27:04 +0900

    page functionsの機能をgo api サーバーに移植 #98

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/stdlib"
@@ -16,6 +18,12 @@ import (
 
 	nsa "github.com/aopontann/nijisanji-songs-announcement"
 )
+
+type ReqBody struct {
+	Song        bool   `json:"song"`
+	Keyword     bool   `json:"keyword"`
+	KeywordText string `json:"keyword_text"`
+}
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
@@ -29,51 +37,26 @@ func main() {
 	sqldb := stdlib.OpenDB(*config)
 	db := bun.NewDB(sqldb, pgdialect.New())
 
-	http.Handle("/", http.FileServer(http.Dir("frontend/public/")))
+	fcm := nsa.NewFCM()
 
-	http.HandleFunc("/is-subscription", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("/is-subscription")
-		if r.Method == http.MethodPost {
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				slog.Error("is-subscription",
-					slog.String("severity", "ERROR"),
-					slog.String("message", err.Error()),
-				)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			exists, err := db.NewSelect().Model((*nsa.User)(nil)).Where("token = ?", string(body)).Exists(ctx)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			if !exists {
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte("NotFound"))
-				return
-			}
-			w.Write([]byte("OK!!"))
+	http.Handle("/", http.FileServer(http.Dir("frontend/dist/")))
+
+	http.HandleFunc("/api/subscription", func(w http.ResponseWriter, r *http.Request) {
+		if len(r.Header["Authorization"]) == 0 {
+			http.Error(w, "NG", http.StatusBadRequest)
 			return
 		}
-	})
+		token := strings.Split(r.Header["Authorization"][0], " ")[1]
+		fmt.Println("token:", token)
 
-	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("access")
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			slog.Error("insert token error",
-				slog.String("severity", "ERROR"),
-				slog.String("message", err.Error()),
-			)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		if r.Method == http.MethodGet {
+			getHandler(w, token, db, ctx)
 			return
 		}
 
 		if r.Method == http.MethodPost {
-			_, err := db.NewInsert().Model(&nsa.User{Token: string(body)}).Ignore().Exec(ctx)
-			if err != nil {
+			var b ReqBody
+			if err = json.NewDecoder(r.Body).Decode(&b); err != nil {
 				slog.Error("insert token error",
 					slog.String("severity", "ERROR"),
 					slog.String("message", err.Error()),
@@ -81,12 +64,65 @@ func main() {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+
+			user := new(nsa.User)
+			err := db.NewSelect().Column("song", "keyword", "keyword_text").Model(user).Where("token = ?", token).Scan(ctx)
+			if err == sql.ErrNoRows {
+				_, err = db.NewInsert().
+					Model(&nsa.User{Token: token, Song: b.Song, Keyword: b.Keyword, KeywordText: b.KeywordText}).
+					Exec(ctx)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				err = fcm.SetTopic(token, b.KeywordText)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				return
+			}
+			if err != nil {
+				slog.Error("select user error",
+					slog.String("severity", "ERROR"),
+					slog.String("message", err.Error()),
+				)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// 変更の場合
+
+			err = fcm.DeleteTopic(token, user.KeywordText)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			err = fcm.SetTopic(token, b.KeywordText)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			_, err = db.NewUpdate().
+				Model(&nsa.User{Token: token, Song: b.Song, Keyword: b.Keyword, KeywordText: b.KeywordText}).
+				WherePK().Exec(ctx)
+			if err != nil {
+				slog.Error("update user error",
+					slog.String("severity", "ERROR"),
+					slog.String("message", err.Error()),
+				)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
 			w.Write([]byte("OK!!"))
 			return
 		}
 
 		if r.Method == http.MethodDelete {
-			_, err := db.NewDelete().Model((*nsa.User)(nil)).Where("token = ?", string(body)).Exec(ctx)
+			_, err := db.NewDelete().Model((*nsa.User)(nil)).Where("token = ?", token).Exec(ctx)
 			if err != nil {
 				slog.Error("delete token error",
 					slog.String("severity", "ERROR"),
@@ -100,11 +136,6 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/key", func(w http.ResponseWriter, r *http.Request) {
-		publicKey := os.Getenv("WEBPUSH_PUBLIC_KEY")
-		w.Write([]byte(publicKey))
-	})
-
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -115,4 +146,34 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getHandler(w http.ResponseWriter, token string, db *bun.DB, ctx context.Context) {
+	user := new(nsa.User)
+	err := db.NewSelect().Column("song", "keyword", "keyword_text").Model(user).Where("token = ?", token).Scan(ctx)
+	if err == sql.ErrNoRows {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write([]byte("No Content"))
+		return
+	}
+	if err != nil {
+		slog.Error("insert token error",
+			slog.String("severity", "ERROR"),
+			slog.String("message", err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("content-Type", "application/json")
+
+	v, err := json.Marshal(&ReqBody{user.Song, user.Keyword, user.KeywordText})
+	if err != nil {
+		slog.Error("insert token error",
+			slog.String("severity", "ERROR"),
+			slog.String("message", err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(v)
 }
