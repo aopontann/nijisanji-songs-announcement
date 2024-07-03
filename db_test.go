ee79a92ccfa6a4dd059c51a3bbd378205b1560a2
aopontann <aopontan0416@gmail.com>
2024-07-03 22:41:10 +0900

    NotExistsVideos のテストを追加 #86

--- db_test.go
+++ db_test.go
@@ -167,7 +167,44 @@ func TestSongVideos5m(t *testing.T) {
 	}
 }
 
-// 動画が登録されているか
+func TestNotExistsVideos(t *testing.T) {
+	bunDB := setup()
+	defer bunDB.Close()
+	db := NewDB(bunDB)
+	yt := NewYoutube(os.Getenv("YOUTUBE_API_KEY"))
+	ctx := context.Background()
+
+	videos := []Video{
+		{ID: "SDr4sxCuMf0", Title: "MV", Duration: "PT4M", Song: false, Viewers: 0, Content: "upcoming", StartTime: time.Date(2024, 6, 27, 12, 10, 0, 0, time.UTC)},
+	}
+	_, err := bunDB.NewInsert().Model(&videos).Exec(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ytVideos, err := yt.Videos([]string{"SDr4sxCuMf0", "YIQFuRXF3tQ"})
+	if err != nil {
+		t.Error(err)
+	}
+
+	notExistsVideos, err := db.NotExistsVideos(ytVideos)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, v := range notExistsVideos {
+		fmt.Println(v.Id)
+	}
+
+	if notExistsVideos[0].Id != "YIQFuRXF3tQ" {
+		t.Error(err)
+	}
+
+	_, err = bunDB.NewDelete().Model(&videos).WherePK().Exec(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func setup() *bun.DB {
 	config, err := pgx.ParseConfig(os.Getenv("DSN"))
