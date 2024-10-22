6778859890839e7e483dd2625ac3f90915561163
aopontann <aopontan0416@gmail.com>
2024-10-22 22:03:52 +0900

    Topic処理のテストを作成 #110

--- fcm_test.go
+++ fcm_test.go
@@ -4,16 +4,14 @@ import (
 	"testing"
 )
 
+const token = ""
+
 func TestSend(t *testing.T) {
 	fcm := NewFCM()
 
 	err := fcm.Notification(
 		"5分後に公開",
-		[]string{
-			"cXPUH6Zl18wtzVCAPI7KaE:APA91bGW4wQ3-k2PgHbEeQzod54NVhK6hwIA9GdfZZyTDt5uE9NJTLSP_QonuDm808bNz8rGHdFCvXeB1_TB9CaEIXnlDAJ5Cu5OW0VyBNV8ezQLaGeDw-eqGVTPavSP2sKvWdfNFktI",
-			"dzrpIshcgHp7FjA09VrFJG:APA91bHioff34_JW3LNXTEjhxNYILdGnGLSuzW54dF3QyzLJNQqD1jTbpoPFdboXLaIP0Oj4okMwyKfipEZri9puNxWGyVZbQSWPKnKi6TzHSJdHakcFOiHbBc1NLSVeT2PUzG9fqHAg",
-			"fVzPqMEQgVNG4ZKCUrf6J-:APA91bHfxrOLfNi0f3mfdnq0-p-FmqOiSzj79IIRlzWsIQoYVFXJiVw-pn8ockeaLfIFRJcwOREqilwyCyx0SNaiaeawuOiLEV_1ObJHr6Dp59GIoIOIuTdf9W5CGrdvoWOw7OQMmGXn",
-		},
+		[]string{},
 		&NotificationVideo{
 			ID:        "Sqpmvv8uulM",
 			Title:     "心予報/歌わせていただきました。",
@@ -25,4 +23,33 @@ func TestSend(t *testing.T) {
 	}
 }
 
-// OPzbUoLxYyE	心予報/歌わせていただきました。	PT3M24S	0	upcoming	false	2024-05-26 13:20:00	2024-05-25 12:00:03	2024-05-25 12:00:03	https://i.ytimg.com/vi/OPzbUoLxYyE/default.jpg
+func TestSetTopic(t *testing.T) {
+	fcm := NewFCM()
+
+	err := fcm.SetTopic(token, "歌枠")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDeleteTopic(t *testing.T) {
+	fcm := NewFCM()
+
+	err := fcm.DeleteTopic(token, "歌枠")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSendTopic(t *testing.T) {
+	fcm := NewFCM()
+
+	err := fcm.TopicNotification("歌枠", &NotificationVideo{
+		ID:        "Sqpmvv8uulM",
+		Title:     "心予報/歌わせていただきました。",
+		Thumbnail: "https://i.ytimg.com/vi/OPzbUoLxYyE/default.jpg",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
